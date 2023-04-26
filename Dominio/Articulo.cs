220886a870f26500eb54ce55eceac841494a98cc
DPinto90 <danielafpinto90@gmail.com>
2023-04-26 10:14:21 -0300

    commit inicial

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Articulo
+    {
+        public int id { set; get; }
+        public string name { set; get; }
+        public string descripcion { set; get; }
+        public float precio { set; get; }
+        public Marca marca { set; get; }
+        public Categoria categoria { set; get; }
+        // public Imagen imagen { set; get; }
+        public bool estado { set; get; }
+
+        public Articulo()
+        {
+            estado = true;
+        }
+    }
+}
+
