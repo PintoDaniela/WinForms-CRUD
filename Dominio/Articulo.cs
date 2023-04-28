dbfa0206fa9e9d6da52b1861ac7c10fbacef20dd
PabloMagna <pablo.magnavachi@alumnos.frgp.utn.edu.ar>
2023-04-28 19:58:03 -0300

    solucionando error merge

--- Dominio/Articulo.cs
+++ Dominio/Articulo.cs
@@ -10,27 +10,19 @@ namespace Dominio
 {
     public class Articulo
     {
-<<<<<<< HEAD
+
         public int Id { set; get; }
         public string Codigo { set; get; }
-=======
->>>>>>> 2b90c7104fc2b006d26a2e007a8799d344c97612
+
         public string Nombre { set; get; }
         public string Descripcion { set; get; }
         public float Precio { set; get; }
         public Marca Marca { set; get; }
         public Categoria Categoria { set; get; }
-<<<<<<< HEAD
         public Imagen Imagen { set; get; }
-        public bool Estado { set; get; }
 
-        public Articulo()
-        {
-            Estado = true;
-        }
-=======
         public List<Imagen> Imagenes { set; get; }
->>>>>>> 2b90c7104fc2b006d26a2e007a8799d344c97612
+
     }
 }
 
--- Dominio/Categoria.cs
+++ Dominio/Categoria.cs
@@ -9,10 +9,10 @@ namespace Dominio
     public class Categoria
     {
         public int Id { set; get; }
-<<<<<<< HEAD
+
         public string Nombre { set; get; }
-=======
+
         public string Name { set; get; }
->>>>>>> 2b90c7104fc2b006d26a2e007a8799d344c97612
+
     }
 }
